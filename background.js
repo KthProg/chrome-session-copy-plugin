233f7a00bcce97d6864d2620e32abe7773ff9852
KthProg <kthprog@gmail.com>
2021-08-23 19:55:43 -0400

    WIP moving to React

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('installed');
+  // TODO: initialize store to have empty list of to and from URLs
 });
---
+++ options-index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import Options from './options';
+
+ReactDOM.render(<Options />, document.getElementById('root'));
